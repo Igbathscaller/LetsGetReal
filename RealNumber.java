a83d2bb7c2a4dfde732ad97ac1c70a6696a9a6a0
Igbathscaller <Igbathscaller@gmail.com>
2020-11-27 21:02:31 -0500

    stuff

--- RealNumber.java
+++ RealNumber.java
@@ -23,38 +23,5 @@ public class RealNumber extends Number{
       return Math.abs(value-other.getValue())*100000<Math.abs(Math.max(value,other.getValue()));
     }
   
-    /*
-    *Return a new RealNumber that has the value of:
-    *the sum of this and the other
-    */
-    public RealNumber add(RealNumber other){
-        //other can be ANY RealNumber, including a RationalNumber
-        //or other subclasses of RealNumber (that aren't written yet)
-        return new RealNumber(value + other.getValue());
-    }
-  
-    /*
-    *Return a new RealNumber that has the value of:
-    *the product of this and the other
-    */
-    public RealNumber multiply(RealNumber other){
-        return new RealNumber(value * other.getValue());
-    }
-  
-    /*
-    *Return a new RealNumber that has the value of:
-    *this divided by the other
-    */
-    public RealNumber divide(RealNumber other){
-        return new RealNumber(value / other.getValue());
-    }
-  
-    /*
-    *Return a new RealNumber that has the value of:
-    *this minus the other
-    */
-    public RealNumber subtract(RealNumber other){
-        return new RealNumber(value - other.getValue());
-    }
   }
   
